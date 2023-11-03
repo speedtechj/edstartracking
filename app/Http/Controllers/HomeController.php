4b8969283afeba8c04ea8f581dd8d84abfccd0b1
samimsaifi <samim.saifi2011@gmail.com>
2023-11-03 14:42:54 -0600

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use DB;
+
+class HomeController extends Controller
+{
+    public function index(){
+     
+      return view('welcome');
+
+    }
+    
+    public function show(Request $request){
+      
+      $invoice1 = $request->input('invoice'); 
+
+     
+      $cms4Invoice = DB::connection('calgarystar_track')
+      ->table('invoicestatuses')
+      ->join('trackstatuses', 'invoicestatuses.trackstatus_id', '=', 'trackstatuses.id')
+      ->join('provincephils', 'invoicestatuses.provincephil_id', '=', 'provincephils.id')
+      ->join('cityphils', 'invoicestatuses.cityphil_id', '=', 'cityphils.id')
+      ->where(function ($query) use ($invoice1) {
+        $query->where('invoicestatuses.generated_invoice', $invoice1)
+            ->orWhere('invoicestatuses.manual_invoice', $invoice1);
+    })
+      ->get();  
+    
+      if($cms4Invoice->count()>0){
+         
+        return view('result2', ['invoice' => $cms4Invoice, 'invoice1'=>$invoice1 ]);
+
+      }  
+     
+
+      $invoiceDetails = DB::connection('calgarystar_track_old')
+      ->table('trackdatas')
+      ->where('invoice', $invoice1)
+      ->get();
+
+      if($invoiceDetails->count() >0){
+        return view('result', ['invoice' => $invoiceDetails, 'invoice1'=>$invoice1]);  
+
+      }else{
+        return view('invoice_not_found',['invoice1'=>$invoice1]);
+      }
+   
+       }
+
+        
+    
+}
